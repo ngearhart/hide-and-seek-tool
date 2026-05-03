f4fa00ea9caa7b7872c64cd8c984832c41252d54
Noah Gearhart <excelsiorvfx@gmail.com>
2026-05-02 21:51:03 -0400

    Fix boundary line

--- src/graphics/boundary.ts
+++ src/graphics/boundary.ts
@@ -1,7 +1,7 @@
 import { Container, Graphics } from "pixi.js";
 import { DrawableElement } from "./base";
 import { rotatePoint, type GameRecord } from "@/utils";
-import { LatLng, type LatLngTuple } from "leaflet";
+import { LatLng, Point, type LatLngTuple } from "leaflet";
 import type { CallbackUtils } from "./pixiOverlay";
 
 export default class Boundary extends DrawableElement {
@@ -26,57 +26,17 @@ export default class Boundary extends DrawableElement {
     }
 
     createWithMap(utils: CallbackUtils): undefined {
-        // So sorry for this garbage implementation
-        if ((this.directionDegrees - 45) % 90 === 0) {
-            const a = this.directionDegrees;
-            let delta = 0.2;
-            let x = this.origin.lat;
-            let y = this.origin.lng;
-            let midPoint = [x, y];
-            let offsetPointEast = [x, y + delta];
-            let offsetPointNorth = [x + delta, y];
-            let offsetPointWest = [x, y - delta];
-            let offsetPointSouth = [x - delta, y];
-            this.trianglePoints = [
-                rotatePoint(offsetPointEast, midPoint, a),
-                rotatePoint(offsetPointNorth, midPoint, a),
-                rotatePoint(offsetPointSouth, midPoint, a),
-            ].map(corner => utils.latLngToLayerPoint(corner));
-        } else if (this.directionDegrees === 0) {
-            this.rectanglePoints = [
-                [this.origin.lat - 1, this.origin.lng],
-                [this.origin.lat + 1, this.origin.lng + 1],
-            ].map(corner => utils.latLngToLayerPoint(corner as LatLngTuple));
-        } else if (this.directionDegrees === 90) {
-            this.rectanglePoints = [
-                [this.origin.lat, this.origin.lng - 1],
-                [this.origin.lat + 1, this.origin.lng + 1],
-            ].map(corner => utils.latLngToLayerPoint(corner as LatLngTuple));
-        } else if (this.directionDegrees === 180) {
-            this.rectanglePoints = [
-                [this.origin.lat - 1, this.origin.lng],
-                [this.origin.lat + 1, this.origin.lng - 1],
-            ].map(corner => utils.latLngToLayerPoint(corner as LatLngTuple));
-        } else if (this.directionDegrees === 270) {
-            this.rectanglePoints = [
-                [this.origin.lat, this.origin.lng + 1],
-                [this.origin.lat - 1, this.origin.lng - 1],
-            ].map(corner => utils.latLngToLayerPoint(corner as LatLngTuple));
-        }
+        let t = utils.latLngToLayerPoint(this.origin);
+        this.graphics.position.x = t.x;
+        this.graphics.position.y = t.y;
+        this.graphics.rotation = Math.PI / 180 * this.directionDegrees;
     }
 
     // 6.178
     draw(utils: CallbackUtils): undefined {
-        if ((this.directionDegrees - 45) % 90 === 0) {
-            
-        } else {
-            this.graphics.rect(
-                Math.min(this.rectanglePoints[0].x, this.rectanglePoints[1].x),
-                Math.min(this.rectanglePoints[0].y, this.rectanglePoints[1].y),
-                Math.abs(this.rectanglePoints[1].x - this.rectanglePoints[0].x), 
-                Math.abs(this.rectanglePoints[1].y - this.rectanglePoints[0].y)
-            ).fill(0x000000);
-        }
+        this.graphics.clear().rect(
+            0, -500, 1000, 1000
+        ).fill(0x000000);
     }
 
     static fromGame(game: GameRecord): Boundary[] {
