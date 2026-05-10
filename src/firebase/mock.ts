f505a938a264412b9b85672fe6e019d0c16c32ac
Noah <excelsiorvfx@gmail.com>
2026-05-10 17:44:22 -0400

    Correct resolution bug (#13)
    * Correct resolution bug
    
    * Oops

--- src/firebase/mock.ts
+++ src/firebase/mock.ts
@@ -15,7 +15,7 @@ const useCurrentUserMock = () => {
 }
 
 const getCurrentUserMock = async() => {
-    if (window.location.origin == "http://localhost:3000") {
+    if (window.location.origin == "http://localhost:3000" || window.location.origin == "http://10.0.2.2:3000") {
         return ref({
             uid: "1wjds6prFGcoHULVaJbSlP9TI0O2",
             photoURL: "",
