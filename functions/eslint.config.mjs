90f1d950c316ef15d714e7f661600aca0b1a3791
Noah Gearhart <excelsiorvfx@gmail.com>
2026-06-28 21:16:45 -0400

    Fix functions deploy

--- functions/eslint.config.mjs
+++ functions/eslint.config.mjs
@@ -3,13 +3,19 @@
  *
  * ESLint configuration file.
  */
+// @ts-check
+
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default [
-  {
-    name: 'app/files-to-lint',
+  ...tseslint.configs.recommended.map(config => ({
+    ...config,
     files: ['**/*.{ts,mts,tsx,vue}'],
-  },
-
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn"
+    }
+  })),
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**, **/lib/**'],
